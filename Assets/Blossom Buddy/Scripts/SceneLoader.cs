ceaa725a9dd755e8d614eca01a2a1cff5262a766
Daniel Zhang <72416560+eminentquasar34@users.noreply.github.com>
2025-10-17 17:22:20 -0700

    Revert "Pruned unneeded files"
    This reverts commit 0906786754a4fb109ad2102c8f2dac4b2806aba2.

--- Assets/Blossom Buddy/Scripts/SceneLoader.cs
+++ Assets/Blossom Buddy/Scripts/SceneLoader.cs
@@ -5,10 +5,21 @@ using UnityEngine.SceneManagement;
 
 public class SceneLoader : MonoBehaviour
 {
+    // Load the Blossom Buddy scene
+    public void LoadBlossomBuddy()
+    {
+        SceneManager.LoadScene("Blossom Buddy");
+    }
+
     // Load the LLM Sample scene
     public void LoadLlmSample()
     {
         SceneManager.LoadScene("LLM Sample");
     }
 
+    // Load the Object Detection Sample scene
+    public void LoadObjectDetectionSample()
+    {
+        SceneManager.LoadScene("Object Detection Sample");
+    }
 }
